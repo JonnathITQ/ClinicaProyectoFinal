0fe88b3b629b8e91ef07e82740067d565b20ffae
Adriel <addrriel@hotmail.com>
2025-10-03 21:43:36 -0500

    Estilos principales acabados, falta modulo paciente y doctor

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Sidebar } from "./components/sidebar/sidebar";
 import { Footer } from "./components/footer/footer";
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Sidebar, Footer],
+  imports: [RouterOutlet, CommonModule, Navbar, Sidebar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ClinicaDental');
+
+  constructor(private router: Router) {}
+
+  get ocultarNavSidebar(): boolean {
+    // Oculta navbar y sidebar si la ruta empieza con /doctorlogin, /pacientelogin, /recuperaciondoctor, /recuperacionpaciente, /registropaciente
+    // O si la ruta contiene 'login', 'recuperacion' o 'register'
+    return this.router.url.startsWith('/doctorlogin')
+      || this.router.url.startsWith('/pacientelogin')
+      || this.router.url.startsWith('/recuperaciondoctor')
+      || this.router.url.startsWith('/recuperacionpaciente')
+      || this.router.url.startsWith('/registropaciente');
+  }
 }
